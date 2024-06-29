586389f86203d50d9adefc15d0c77f3b63d64d52
Georgi <gogolino1982@gmail.com>
2024-06-29 14:01:36 +0300

    fixed unit tests

--- tests/unit/add-grades.tests.js
+++ tests/unit/add-grades.tests.js
@@ -1,17 +1,17 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
 
-suite('Add Grades page', function() {
-  test('Page title', async function() {
+suite('Add Grades page', function () {
+  test('Page title', async function () {
     let res = await fetch("http://localhost:8888/Add-Grade");
     let body = await res.text();
     assert.ok(body.includes("<h1>Add New Grade</h1>"));
   });
 
-  test('Grade HTML form', async function() {
+  test('Grade HTML form', async function () {
     let res = await fetch("http://localhost:8888/Add-Grade");
     let body = await res.text();
-    
+
     let subjectFieldFound = body.includes('<input id="subject" type="text" name="subject"/>');
     assert.ok(subjectFieldFound, "Field 'subject' is missing");
 
@@ -22,7 +22,7 @@ suite('Add Grades page', function() {
     assert.ok(buttonAddFound, "Button [Add] is missing");
   });
 
-  test('Add valid grade', async function() {
+  test('Add valid grade', async function () {
     let res = await fetch(
       "http://localhost:8888/Add-Grade",
       {
@@ -35,12 +35,12 @@ suite('Add Grades page', function() {
     );
     let body = await res.text();
     let gradesReturned = body.includes(
-		"<ul><li>English (4.50)</li><li>Math (5.50)</li><li>Programming Basics (6.00)</li><li>Physics (3.90)</li></ul>");
+      "<ul><li>English (5.50)</li><li>Math (4.50)</li><li>Programming Basics (6.00)</li><li>Physics (3.90)</li></ul>");
     assert.ok(gradesReturned, "Add grade failed");
   });
 
-  test('Add invalid grade', async function() {
-     let res = await fetch(
+  test('Add invalid grade', async function () {
+    let res = await fetch(
       "http://localhost:8888/Add-Grade",
       {
         method: 'POST',
@@ -56,7 +56,7 @@ suite('Add Grades page', function() {
 
     res = await fetch("http://localhost:8888/");
     body = await res.text();
-	  assert.ok(body.includes("Grades: <b>3</b>"), 
-		"Add invalid grade should not change the grades count");
+    assert.ok(body.includes("Grades: <b>3</b>"),
+      "Add invalid grade should not change the grades count");
   });
 });
