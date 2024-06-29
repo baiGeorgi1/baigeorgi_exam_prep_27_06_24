0d080570ff3c25f56e11e845b2e12d2453b40047
Georgi <gogolino1982@gmail.com>
2024-06-29 14:14:51 +0300

    UI tests fixed

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ gradesController.setup(app, gradesList);
 
 let port = process.argv[2];
 if (!port) port = process.env['PORT'];
-if (!port) port = 8090;
+if (!port) port = 8080;
 
 app.listen(port, () => {
   console.log(`App started. Listening at http://localhost:${port}`);
 })
-.on('error', function(err) {
-  if (err.errno === 'EADDRINUSE')
-    console.error(`Port ${port} busy.`);
-  else 
-    throw err;
-});
+  .on('error', function (err) {
+    if (err.errno === 'EADDRINUSE')
+      console.error(`Port ${port} busy.`);
+    else
+      throw err;
+  });
